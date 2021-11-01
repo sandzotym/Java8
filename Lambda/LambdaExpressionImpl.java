620f414e25ea71c34fb4eb422c873ee92720ca6b
sandzotym <93154062+sandzotym@users.noreply.github.com>
2021-11-01 20:23:03 +0530

    Add files via upload

---
+++ Lambda/LambdaExpressionImpl.java
@@ -0,0 +1,8 @@
+package com.lambda;
+
+public class LambdaExpressionImpl {
+    public static void main(String[] args){
+        LambdaExpression aLambdaExpression = () -> System.out.println("Hello World");
+        aLambdaExpression.abc();
+    }
+}
